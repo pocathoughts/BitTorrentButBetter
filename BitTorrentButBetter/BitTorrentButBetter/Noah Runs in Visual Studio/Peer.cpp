e16428fb62535088bfa94f2bbda85f62164fad92
LARA DEDIC <laradedic@ufl.edu>
2016-10-17 23:35:34 -0400

    pseudo for peer connections

--- BitTorrentButBetter/BitTorrentButBetter/Noah Runs in Visual Studio/Peer.cpp
+++ BitTorrentButBetter/BitTorrentButBetter/Noah Runs in Visual Studio/Peer.cpp
@@ -1,12 +1,12 @@
 #include "stdafx.h"
 #include "Peer.h"
 
-Peer::Peer() 
+Peer::Peer()
 {
 	optomisticallyUnchokedNeighbor = NULL;
 }
 
-void Peer::SendHandshakeMessage(Peer * otherPeer) 
+void Peer::SendHandshakeMessage(Peer * otherPeer)
 {
 
 }
@@ -14,21 +14,26 @@ void Peer::SendActualMessage(int type)
 {
 
 }
-void Peer::EstablishConnection(Peer * otherPeer) 
+void Peer::EstablishConnection(Peer * otherPeer)
 {
-
+	//go to peerinfo.cfg to establish connections w/ peers above it
+	//after establishing TCP connection, send handshake message to other peer
 }
-void Peer::ReceiveHandshakeMessage(Message m) 
+void Peer::ReceiveHandshakeMessage(Message m)
 {
-
+	//check handshake header peerID and make sure its expected
 }
 void Peer::ReceiveActualMessage(Message m, Peer * sender)
 {
-
+	//bitfield message after handshake: check for new pieces, send interested or
+	//not interested
 }
-void Peer::DeterminePreferredNeighbors() 
+void Peer::DeterminePreferredNeighbors()
 {
-
+	//# of preferred neighbors in common.cfg
+	//this functioned should be called after every p seconds in common.cfg
+	//check download rate of peers from prev unchoking interval
+	//send unchoke/choke messages to selected peers
 }
 void Peer::DetermineOptomisticallyUnchokedNeighbor()
 {
@@ -36,7 +41,7 @@ void Peer::DetermineOptomisticallyUnchokedNeighbor()
 }
 void Peer::DetermineInterested()
 {
-
+	//if peer has received message type = 'bitfield' or 'have' AND bitfields differ
 }
 Peer::~Peer()
 {
