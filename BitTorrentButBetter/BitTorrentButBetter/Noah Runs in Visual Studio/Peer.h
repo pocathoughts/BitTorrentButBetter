70cc5e301a50204938a13478d16f52182ec1487e
Pocathoughts <christinemoore321@gmail.com>
2016-10-18 17:24:55 -0400

    added new fields

--- BitTorrentButBetter/BitTorrentButBetter/Noah Runs in Visual Studio/Peer.h
+++ BitTorrentButBetter/BitTorrentButBetter/Noah Runs in Visual Studio/Peer.h
@@ -6,19 +6,28 @@
 class Peer
 {
 public:
-	Peer();
-	void SendHandshakeMessage(Peer * otherPeer);
-	void SendActualMessage(int type);
-	void EstablishConnection(Peer * otherPeer);
-	void ReceiveHandshakeMessage(Message m);
-	void ReceiveActualMessage(Message m, Peer * sender);
-	void DeterminePreferredNeighbors();
-	void DetermineOptomisticallyUnchokedNeighbor();
-	void DetermineInterested();
-	void CreateHandshakeMessage();
-	~Peer();
+    Peer();
+    void SendHandshakeMessage(Peer * otherPeer);
+    void SendActualMessage(int type);
+    void EstablishConnection(Peer * otherPeer);
+    void ReceiveHandshakeMessage(Message m);
+    void ReceiveActualMessage(Message m, Peer * sender);
+    void DeterminePreferredNeighbors();
+    void DetermineOptomisticallyUnchokedNeighbor();
+    void DetermineInterested();
+    void CreateHandshakeMessage();
+    ~Peer();
 private:
-	std::vector<Peer*> otherPeers;
-	Peer * optomisticallyUnchokedNeighbor;
+    std::vector<Peer*> otherPeers;
+    Peer * optomisticallyUnchokedNeighbor;
+    int ownPeerID;
+    int neighborID;
+    // Socket neighborSocket
+    // need some kind of input and output stream to send the data
+    // 1 is true for below 0 is false
+    // below are the boolean variables
+    int peerIsChoking;
+    int hasFile;
+    int sentHandshake;
 };
 
