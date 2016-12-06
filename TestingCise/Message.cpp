aa5025947087c10bb3be381388767b748bf960b7
Noah Presser <Noah Presser>
2016-12-06 14:51:54 -0500

    all the doges

--- TestingCise/Message.cpp
+++ TestingCise/Message.cpp
@@ -234,8 +234,8 @@ std::vector<OURBYTE> Message::GenerateBitfieldPayload(bool hasPiece, std::vector
 		}
 		lib->printBitsInByte(currentByte);
 		bitfieldPayload.push_back(currentByte);
-		std::cout << "push back done, bits: ";
-		lib->printBitsInByte(bitfieldPayload.back());
+		std::cout << "push back done, bytestream: ";
+		lib->printByteStream(bitfieldPayload);
 	}
 	std::cout << "gen bitfieldpayload 5\n";
 
