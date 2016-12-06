ba0089fef06a669402af3a44ac413abc14b52566
Noah Presser <Noah Presser>
2016-12-06 04:14:18 -0500

    this gon get old real fast

--- TestingCise/Peer.cpp
+++ TestingCise/Peer.cpp
@@ -402,7 +402,7 @@ void Peer::AwaitMessage(int sockfd)
 	char message[256];
 	//waits for a return handshake message
 	bzero(message, 255);
-	n = read(sockfd, message, 256);
+	n = recv(sockfd, message, 256, 0);
 	std::cout << "whoa\n";
 	std::vector<OURBYTE> messageStream = lib->GetByteStreamFromString(message);
 	int type = lib->GetMessageTypeFromStream(messageStream);
