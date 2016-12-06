e5f2219c51fd247f7fbc2b9748bcb5d0fd27b87c
Noah Presser <Noah Presser>
2016-12-05 22:18:09 -0500

    nvmd reverting ugh uf y u do dis 2

--- TestingCise/Peer.cpp
+++ TestingCise/Peer.cpp
@@ -664,8 +664,7 @@ void Peer::startClientLinux(char * hostName, int otherPeerID)
 	if (sockfd < 0)
 		error("ERROR opening socket");
 
-
-	server = getaddrinfo(hostName);
+	server = gethostbyname(hostName);
 	if (server == NULL) {
 		fprintf(stderr, "ERROR, no such host\n");
 		exit(0);
