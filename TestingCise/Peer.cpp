5555f88f1e292dabcac9862ee13c100d90830392
Noah Presser <Noah Presser>
2016-12-07 18:09:03 -0500

    remove unecessary comments and made filename check

--- TestingCise/Peer.cpp
+++ TestingCise/Peer.cpp
@@ -33,7 +33,7 @@ Peer::Peer(int _peerID, char * _hostName, int _portNum, bool _fileComplete, std:
 	//end evil
 
 	int a = peerID;
-	std::string a = std::to_string(a);
+	std::string baa = std::to_string(a);
 	subdir = "peer_"  + std::to_string(peerID);
 	subDirAndFile = "peer_" + std::to_string(peerID) + "/" + fileName;
 	//int a = peerID;
